be6fccd7fe7d8ca6a6e437d91fad80a49b6ad148
dipalimagare <dipalimagare2026@gmail.com>
2021-09-30 21:19:26 +0530

    Added Uc3 Put Full Sign

--- src/test/java/com/parkingLotTest/ParkingLotTest.java
+++ src/test/java/com/parkingLotTest/ParkingLotTest.java
@@ -17,29 +17,53 @@ public class ParkingLotTest {
     }
 
     @Test
-    public void givenVehicle_WhenPark_ShouldReturnTrue() {
-        boolean parkingStatus = parkingLot.parkVehicle(vehicle);
-        Assertions.assertTrue(parkingStatus);
+    public void givenVehicle_WhenPark_ShouldReturnTrue() throws ParkingLotException {
+        parkingLot.parkVehicle(vehicle);
+        boolean isParked = parkingLot.isParked();
+        Assertions.assertTrue(isParked);
     }
 
     @Test
-    public void givenVehicle_WhenAlreadyParked_ShouldReturnFalse() {
+    public void givenVehicleIfParked_WhenUnParked_ShouldReturnTrue() throws ParkingLotException {
         parkingLot.parkVehicle(vehicle);
-        boolean parkingStatus = parkingLot.parkVehicle(vehicle);
+        parkingLot.unParkVehicle(vehicle);
+        boolean isUnParked = parkingLot.isUnParked();
+        Assertions.assertTrue(isUnParked);
+    }
+
+    @Test
+    public void givenVehicle_WhenAlreadyParked_ShouldReturnFalse() throws ParkingLotException {
+        parkingLot.parkVehicle(vehicle);
+        boolean parkingStatus = parkingLot.isUnParked();
         Assertions.assertFalse(parkingStatus);
     }
 
     @Test
-    public void givenVehicleIfParked_WhenUnParked_ShouldReturnTrue() {
+    public void givenVehicleIfParked_AndGivenDifferentVehicleToUnPark_ShouldReturnFalse() throws ParkingLotException {
         parkingLot.parkVehicle(vehicle);
-        boolean parkingStatus = parkingLot.unParkVehicle(vehicle);
-        Assertions.assertTrue(parkingStatus);
+        boolean parkingStatus = parkingLot.isUnParked();
+        Assertions.assertFalse(parkingStatus);
     }
 
     @Test
-    public void givenVehicleIfParked_AndGivenDifferentVehicleToUnPark_ShouldReturnFalse() {
-        parkingLot.parkVehicle(vehicle);
-        boolean parkingStatus = parkingLot.unParkVehicle(vehicle2);
-        Assertions.assertFalse(parkingStatus);
+    public void givenVehicleToPark_IfLotFull_ShouldThrowException() {
+        try {
+            parkingLot.parkVehicle(vehicle);
+            parkingLot.parkVehicle(vehicle2);
+        } catch (ParkingLotException e) {
+            System.out.println(e.type);
+            Assertions.assertEquals(e.type, ParkingLotException.ExceptionType.LOT_FULL);
+        }
+    }
+
+    @Test
+    public void givenVehicleToUnPark_WhenNull_ShouldThrowException() {
+        try {
+            parkingLot.parkVehicle(vehicle);
+            parkingLot.unParkVehicle(null);
+        } catch (ParkingLotException e) {
+            System.out.println(e.type);
+            Assertions.assertEquals(e.type, ParkingLotException.ExceptionType.NO_SUCH_VEHICLE);
+        }
     }
 }
